6d8092b3710f38b1249c83bb9f41a1ef12c14655
YounesMoukhlij <123767564+YounesMoukhlij@users.noreply.github.com>
2024-01-19 01:25:37 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: younesmoukhlij <younesmoukhlij@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 14:39:48 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/01/19 01:21:12 by younesmoukh      ###   ########.fr       */
+/*   Updated: 2024/01/19 01:25:23 by younesmoukh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ void	show(void)
 	system("leaks so_long");
 }
 
+
 int	main(int ac, char **av)
 {
 	t_solong variable;
-	
+
 	// atexit(show);
 	ft_parse(ac, av[1]);
 	printf("%d\n", (variable.map.player));
